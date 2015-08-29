d2038d5ea26c4bcbf95a1f373a6233516e12ec64
Paul Ruvolo <paullundyruvolo@gmail.com>
2015-08-29 17:06:50 -0400

    allow different UDP streaming ports

--- src/gscam/scripts/setup_udp_stream.py
+++ src/gscam/scripts/setup_udp_stream.py
@@ -6,17 +6,18 @@ from os import system
 
 rospy.init_node('setup_udp_stream')
 
+receive_port = rospy.get_param('~receive_port')
 host = rospy.get_param('~host')
 port = 10003
 size = 1024
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((host,port))
-s.send('hello')
+s.send(str(receive_port) + "\n")
 all_data = ""
 while not all_data.endswith('\n'):
 	data = s.recv(size)
 	all_data += data
 s.close()
 
-system('hping3 -c 1 -2 -s 5000 -p ' + all_data.strip() + ' ' + host)
+system('hping3 -c 1 -2 -s ' + str(receive_port) + ' -p ' + all_data.strip() + ' ' + host)
 print 'Received:', all_data
